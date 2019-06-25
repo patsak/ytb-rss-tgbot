6c00081b4d4369782e91ab112b736c3c2486ded2
Konstantin Privezentsev <privezentsev@gmail.com>
2019-06-26 02:48:43 +0300

    sceleton

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/patsak/ytb-rss-tgbot
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.5-0.20190524110549-cea05bfc443c+incompatible
+	github.com/rylio/ytdl v0.5.2-0.20190315183053-1f14ef2e151a
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,94 @@
+package  main
 
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"os/exec"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/rylio/ytdl"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	destDir = flag.String("dest", "content", "destination dir")
+	token = flag.String("token", "", "bot token")
+)
+
+func main() {
+	flag.Parse()
+
+	bot, err := tgbotapi.NewBotAPI(*token)
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	fmt.Println("bot started")
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		url := &url.URL{}
+		url, err = url.Parse(update.Message.Text)
+		if err != nil {
+			logrus.Error(err.Error())
+		}
+		info, err := ytdl.GetVideoInfoFromURL(url)
+		if err != nil {
+			logrus.Error(err.Error())
+			continue
+		}
+		file, err := os.Create(destVideo(info.ID))
+		if err != nil {
+			logrus.Error(err.Error())
+			continue
+		}
+		err = info.Download(info.Formats[0], file)
+		_ = file.Close()
+		if err != nil {
+			logrus.Error(err.Error())
+			continue
+		}
+
+		cmd := exec.Command("ffmpeg", "-i", destVideo(info.ID), "-f", "mp3", "-y", "-ab", "64000", "-vn", destAudio(info.ID))
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			logrus.Println("%s", string(output))
+			logrus.Error(err.Error())
+			continue
+		}
+		sendFile, err := os.OpenFile(destAudio(info.ID), os.O_RDONLY, os.ModePerm)
+		if err != nil {
+			logrus.Error(err.Error())
+			continue
+		}
+		reader := tgbotapi.FileReader{
+			info.Title,
+			sendFile,
+			-1,
+
+		}
+		config := tgbotapi.NewAudioUpload(update.Message.Chat.ID, reader)
+
+		_, err = bot.Send(config)
+		if err != nil {
+			logrus.Error(err.Error())
+		}
+	}
+
+}
+
+func destVideo(id string) string {
+	return *destDir + "/" + id + ".mp4"
+}
+func destAudio(id string) string {
+	return *destDir + "/" + id + ".mp3"
+}
